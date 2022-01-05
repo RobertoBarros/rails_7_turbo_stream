cd7be1d2cf5b645a76941aa852acbcba74ac66fe
Roberto Barros <roberto.f.barros@gmail.com>
2022-01-05 20:19:04 -0300

    Turbo Stream Implementation

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,7 +3,24 @@ class BidsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     value = params[:bid_value]
-    Bid.create!(product: product, user: current_user, value: value)
-    redirect_to product_path(product)
+    bid = Bid.create!(product: product, user: current_user, value: value)
+
+
+    respond_to do |format|
+      format.html { redirect_to product_path(product) }
+      format.turbo_stream do
+        bid.broadcast_prepend_to product
+
+        product.broadcast_update_to product, html: view_context.number_to_currency(product.final_price), target: 'current-price'
+
+        # product.broadcast_update_to product,
+        #                            partial: 'products/product_info',
+        #                            locals: {bid: Bid.new, current_user: nil}
+      end
+
+    end
+
+
+
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,9 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :bids, dependent: :destroy
 
+  after_create_commit { broadcast_prepend_to :products }
+  after_update_commit { broadcast_update_to :products }
+  after_destroy_commit { broadcast_remove_to :products }
+
   scope :in_auction, -> { where(buyer: nil) }
 end
